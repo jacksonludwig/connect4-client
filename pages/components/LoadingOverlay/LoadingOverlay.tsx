46cf4f9ed1fde27955fd1e1d09041588f480be7e
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-07-09 00:22:27 -0400

    change spinner

--- pages/components/LoadingOverlay/LoadingOverlay.tsx
+++ pages/components/LoadingOverlay/LoadingOverlay.tsx
@@ -17,7 +17,13 @@ const LoadingOverlay = ({ enabled }: Props): ReactElement => {
           w="100%"
           bg="rgba(0, 0, 0, 0.5)"
         >
-          <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+          <Spinner
+            thickness="0.3rem"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
         </Flex>
       )}
     </>
