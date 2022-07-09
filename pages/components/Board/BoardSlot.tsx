b4f19147b87ae7896fd0cbe2f93f9c45a420c40b
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-07-09 10:38:45 -0400

    feat: responsive board design

--- pages/components/Board/BoardSlot.tsx
+++ pages/components/Board/BoardSlot.tsx
@@ -6,10 +6,24 @@ type BoardSlotProps = {
   filledBy: PlayerToken;
 };
 
+const outResponseiveSize = { base: '3.00rem', md: '4.5rem', lg: '6.00rem', xl: '7.75rem' };
+const innerResponsiveSize = { base: '2.75rem', md: '3.7rem', lg: '5.5rem', xl: '7.00rem' };
+
 const BoardSlot = ({ filledBy }: BoardSlotProps): ReactElement => {
   return (
-    <Flex justify="center" align="center" h="5.5rem" w="5.5rem" bg="lightblue">
-      <Box h="4.75rem" w="4.75em" borderRadius="100%" bg={`${PLAYER_COLOR_MAP[filledBy]}`}></Box>
+    <Flex
+      justify="center"
+      align="center"
+      h={outResponseiveSize}
+      w={outResponseiveSize}
+      bg="lightblue"
+    >
+      <Box
+        h={innerResponsiveSize}
+        w={innerResponsiveSize}
+        borderRadius="100%"
+        bg={`${PLAYER_COLOR_MAP[filledBy]}`}
+      ></Box>
     </Flex>
   );
 };
